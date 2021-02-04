c6d4dcfccdd25b909727e5d82126509f89a585d3
dunkdunkdunk <dunk.soravit@gmail.com>
2021-02-04 13:12:05 +0700

    **Static Website can't request!!***

---
+++ Features.js
@@ -0,0 +1,27 @@
+let word = []
+
+function submit() {
+    let input = document.getElementById("text").value
+    if (input == '' || input == ' ' || input == '   ' || input.includes('<')) {
+        alert("Error")
+    }
+    else {
+        word.push(' ' + input)
+        document.getElementById("display").innerHTML = word
+        document.getElementById("text").value = null
+    }
+}
+/*
+function reveal() {
+    let xhr = new XMLHttpRequest()
+    let url = 'https://jsonplaceholder.typicode.com/posts'
+    xhr.open('GET',url)
+    xhr.send()
+    document.write(xhr.response)
+}*/
+
+document.querySelector('#text').addEventListener('keydown', (e) => {
+    if (e.key==='Enter') {
+        submit()
+    }
+})
